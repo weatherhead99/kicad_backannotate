554ab2124487ed7d201e682a5fd138527e4927dc
Dan Weatherill <plasteredparrot@gmail.com>
2018-01-12 06:22:03 +0000

    distutils file

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 06:18:23 2018
+
+@author: danw
+"""
+
+from distutils.core import setup
+
+setup(
+      name="kicad_backannotate",
+      version="0.0.1dev",
+      packages=["kicad_backannotate"]
+      
+      )
